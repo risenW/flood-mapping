cddb177c1270f71728b136ccc237ccdb434fb547
risenW <risingodegua@gmail.com>
2020-02-18 00:52:10 +0100

    Add abia geojson file

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,20 +2,14 @@
 
 var data = [{
     type: "choroplethmapbox", name: "US states", geojson:
-        "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json",
-    locations: ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN",
-        "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-        "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA",
-        "WA", "WV", "WI", "WY"],
-    z: [141, 140, 155, 147, 132, 146, 151, 137, 146, 136, 145, 141, 149, 151, 138, 158, 164, 141, 146,
-        145, 142, 150, 155, 160, 156, 161, 147, 164, 150, 152, 155, 167, 145, 146, 151, 154, 161, 145, 155,
-        150, 151, 162, 172, 169, 170, 151, 152, 173, 160, 176],
-    zmin: 25, zmax: 280, colorbar: { y: 0, yanchor: "bottom", title: { text: "US states", side: "right" } }
+        "https://github.com/risenW/education4all/blob/master/public/data/pry_sch.json",
+   
+    zmin: 25, zmax: 280, colorbar: { y: 0, yanchor: "bottom", title: { text: "Abia LGA", side: "right" } }
 }
 ];
 
 var layout = {
-    mapbox: { style: "dark", center: { lon: -110, lat: 50 }, zoom: 2.0 }, width: 1000, height:
+    mapbox: { style: "dark", center: { lon: 9, lat: 7 }, zoom: 5.0 }, width: 1000, height:
         800, margin: { t: 0, b: 0 }
 };
 
