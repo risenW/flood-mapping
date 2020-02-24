84d4c35925bb1a5d85403ae1139d34bca7cb5692
risenW <risingodegua@gmail.com>
2020-02-24 13:49:34 +0100

    Remove routes

--- functions/index.js
+++ functions/index.js
@@ -19,41 +19,9 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-
-
-let data = fs.readFileSync('data/farmlands.geojson');
-let farms = JSON.parse(data);
-
-let data2 = fs.readFileSync('data/oyo-boundries.geojson');
-let oyo_bound = JSON.parse(data2);
-
-
 app.get('/', (req, res) => {
     res.render('index')
 })
-
-app.get('/animated', (req, res) => {
-    res.render('animated')
-})
-
-app.get('/flood', (req, res) => {
-    res.render('flood')
-})
-
-// Sending data
-app.get('/farms', (req, res) => {
-    res.send(farms)
-})
-
-app.get('/oyo-bound', (req, res) => {
-    res.send(oyo_bound)
-})
-
-// End Sending data
-
-// app.get('**', (req, res) => {
-// res.status(404).redirect('404.html')
-// })
 
 
 exports.app = functions.https.onRequest(app);
